4d9660636a7e6e08e79759f5cd8c4d24aea11e80
DouglasOttoSf <douglas.davila@objectedge.com>
2022-05-12 23:51:18 -0300

    Initial commit

--- src/notas/App.java
+++ src/notas/App.java
@@ -10,6 +10,9 @@ public class App {
         Scanner scan = new Scanner(System.in);
 
         String nome;
+        int aluId;
+        int trimestre;
+        float notaFinal;
 
         int opcao = 0;
         while (opcao >= 0) {
@@ -22,26 +25,38 @@ public class App {
             switch (opcao) {
                 case 1:
                     System.out.println("Informe o nome do aluno:");
+                    scan.nextLine();
                     nome = scan.nextLine();
                     stud.inserirNomes(nome);
+                    stud.retornaNomes();
+                    com.aguardaInput();
                     break;
 
                 case 2:
                     System.out.println("Escolha o aluno:");
                     stud.retornaNomes();
                     System.out.println("Digite o número do aluno.");
-                    int aluId = scan.nextInt();
-                    aluId--;
+                    aluId = scan.nextInt();
                     System.out.println("Informe o trimestre.");
-                    int trimestre = scan.nextInt();
-                    trimestre--;
-                    System.out.println("Informe a nota do " + trimestre+1 + "º trimestre.");
+                    trimestre = scan.nextInt();
+                    System.out.println("Informe a nota do " + trimestre + "º trimestre.");
                     float nota = scan.nextFloat();
+                    trimestre--;
                     stud.inserirNota(aluId, trimestre, nota);
+                    com.aguardaInput();
                     break;
 
                 case 3:
-                    /* elev.sobe(); */
+                    System.out.println("Escolha o aluno:");
+                    stud.retornaNomes();
+                    System.out.println("\nDigite o número do aluno.");
+                    aluId = scan.nextInt();
+                    com.limparTela();
+                    notaFinal = stud.calcularNota(aluId);
+                    System.out.println("A nota final é: " + notaFinal);
+                    stud.checarResultado(aluId);
+                    stud.retornaNotasAluno(aluId);
+                    com.aguardaInput();
                     break;
 
                 case 4:
--- src/notas/Student.java
+++ src/notas/Student.java
@@ -1,53 +1,97 @@
 package notas;
 
-import java.io.*;
-
 public class Student {
+
+    Commons com = new Commons();
 
     private String nomes[] = new String[3];
     private float notas[][] = new float[3][3];
+    private float notaFinal[][] = new float[3][1];
     private int aluContagem = 0;
+    private int maxAlunos = 3;
 
     public void inserirNomes(String nome){
-        this.nomes[aluContagem] = nome; 
-        aluContagem++;
+        if (aluContagem >= 3){
+            System.out.println("O limite de " + this.maxAlunos + " alunos foi alcançado.");
+            System.out.println("Não é possível incluir outro(s) contato(s).");
+        } else {
+            this.nomes[aluContagem] = nome; 
+            aluContagem++;
+        }
     }
 
     public void retornaNomes(){
-        for (int i = 0; i < aluContagem; i++){
+        for (int i = 0; i < this.aluContagem; i++){
             if (this.nomes[i] != null){
                 String nome = this.nomes[i];
                 System.out.println(i + " " + nome);
             }
         }
+        System.out.println("\nAlunos totais: " + this.aluContagem + "/3");
     }
 
     public void inserirNota(int aluId, int trimestre, float nota){
-        notas[aluId][trimestre] = nota;
+        this.notas[aluId][trimestre] = nota;
     }
 
     public void retornaNotasAluno(int aluId){
         float n1 = notas[aluId][0];
         float n2 = notas[aluId][1];;
-        float n3 = notas[aluId][2];;
-        float total;
+        float n3 = notas[aluId][2];
 
         if (n1 != 0f){
             System.out.println("1º Trimestre: " + n1);
+        } else {
+            System.out.println("1º Trimestre: Nota não contabilizada."); 
         }
-
-        
+        if (n2 != 0f){
+            System.out.println("2º Trimestre: " + n2);
+        } else {
+            System.out.println("2º Trimestre: Nota não contabilizada."); 
+        }
+        if (n3 != 0f){
+            System.out.println("3º Trimestre: " + n3);
+        } else {
+            System.out.println("3º Trimestre: Nota não contabilizada."); 
+        }
     }
 
     public float calcularNota(int aluId){
-        float nota1 = notas[aluId][0];
-        float nota2 = notas[aluId][1];
-        float nota3 = notas[aluId][2];
+        float nota1 = this.notas[aluId][0];;
+        float nota2 = this.notas[aluId][1];;
+        float nota3 = this.notas[aluId][2];;
         float notaFinal;
 
+        if (nota1 != 0f){
+
+        } else {
+            nota1 = 0;
+        }
+
+        if (nota2 != 0f){
+
+        } else {
+            nota3 = 0;
+        }
+
+        if (nota3 != 0f){
+
+        } else {
+            nota3 = 0;
+        }
+
         notaFinal = ((nota1/100f)*3) + ((nota2/100f)*3.5f)  + ((nota3/100f)*3.5f);
+        this.notaFinal[aluId][0] = notaFinal;
 
         return notaFinal;
+    }
+
+    public void checarResultado(int aluId){
+        if (this.notaFinal[aluId][0] < 60f) {
+            System.out.println("O aluno está reprovado!");
+        } else {
+            System.out.println("O aluno está aprovado!");
+        }
     }
 
 }
